347d132f1a47740fa3c160d131251db7d519cbee
Bumrang <maximkuznet@gmail.com>
2018-01-20 15:13:46 -0800

    Added free roam camera

---
+++ Assets/hover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MeshFilter[] meshes = gameObject.GetComponentsInChildren<MeshFilter>();
+		/*foreach (MeshFilter child in meshes)
+		{
+		}*/
+	}
+}
